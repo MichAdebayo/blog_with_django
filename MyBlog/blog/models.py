e08d2febd7bc02d0f16cf3b22dce1dfa7f5305b9
MichAdebayo <adebayomichael4@gmail.com>
2025-09-04 20:39:50 +0200

    fix: Add docstrings for ArticleCategory and Article models

--- MyBlog/blog/models.py
+++ MyBlog/blog/models.py
@@ -3,11 +3,13 @@ from django.db import models
 # Create your models here.
 
 class ArticleCategory(models.Model):
+    """Model representing a category of articles."""
     name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
     
 class Article(models.Model):
+    """Model representing a blog article."""
     titre = models.CharField(max_length=200)
     contenu = models.TextField()
     date_publication = models.DateField(auto_now_add=True)
